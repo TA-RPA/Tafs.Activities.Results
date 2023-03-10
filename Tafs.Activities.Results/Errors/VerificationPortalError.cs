d425993e996d2c6279c5fcb5cb014813eb3d18f3
Devin Duanne <dduanne@tafs.com>
2023-03-10 16:42:47 -0600

    Add Verification Portal Error

---
+++ Tafs.Activities.Results/Errors/VerificationPortalError.cs
@@ -0,0 +1,37 @@
+﻿//
+//  VerificationPortalError.cs
+//
+//  Author:
+//       TAFS RPA Developers
+//
+//  Copyright (c) 2021 TAFS, LLC.. All rights reserved.
+//
+
+namespace Tafs.Activities.Results.Errors
+{
+    /// <summary>
+    /// Represents an error which occurred while verifying a batch against a debtor portal.
+    /// </summary>
+    public sealed class VerificationPortalError : ResultError
+    {
+        /// <summary>
+        /// Gets the <see cref="VerificationPortalStatus"/> for this error.
+        /// </summary>
+        public VerificationPortalStatus VerificationStatus { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VerificationPortalError"/> class.
+        /// </summary>
+        /// <param name="verificationStatus">The status to report for this error.</param>
+        /// <param name="details">The reason behind the failure.</param>
+        public VerificationPortalError
+        (
+            VerificationPortalStatus verificationStatus,
+            string details
+        )
+            : base($"Verifying the batch failed: {details}")
+        {
+            VerificationStatus = verificationStatus;
+        }
+    }
+}
---
+++ Tafs.Activities.Results/Errors/VerificationPortalStatus.cs
@@ -0,0 +1,43 @@
+﻿//
+//  VerificationPortalStatus.cs
+//
+//  Author:
+//       TAFS RPA Developers
+//
+//  Copyright (c) 2021 TAFS, LLC.. All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tafs.Activities.Results.Errors
+{
+    /// <summary>
+    /// Represents a final status as reported to the Verification Portal endpoint.
+    /// </summary>
+    public enum VerificationPortalStatus
+    {
+        /// <summary>
+        /// The record was verified successfully.
+        /// </summary>
+        Verified,
+
+        /// <summary>
+        /// The record was not found or the client did not match.
+        /// </summary>
+        RejectedFraud,
+
+        /// <summary>
+        /// The invoice was found but the amount did not match.
+        /// </summary>
+        RejectedPaymentIssue,
+
+        /// <summary>
+        /// A bot exception occurred.
+        /// </summary>
+        Exception
+    }
+}
