b65b20ec7c5f7f844f17defe66acd2d3c433041f
Devin Duanne <dduanne@tafs.com>
2023-03-13 09:53:46 -0500

    Add constructor to Verification Portal Error

--- Tafs.Activities.Results/Errors/VerificationPortalError.cs
+++ Tafs.Activities.Results/Errors/VerificationPortalError.cs
@@ -33,5 +33,26 @@ namespace Tafs.Activities.Results.Errors
         {
             VerificationStatus = verificationStatus;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VerificationPortalError"/> class.
+        /// </summary>
+        /// <remarks>
+        /// Sets the verification error to '<paramref name="expected"/>' does not equal '<paramref name="actual"/>'. <paramref name="comment"/>.
+        /// </remarks>
+        /// <param name="verificationStatus">The status to report for this error.</param>
+        /// <param name="expected">The expected value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="comment">An additional comment appended to the end.</param>
+        public VerificationPortalError
+        (
+            VerificationPortalStatus verificationStatus,
+            string expected,
+            string actual,
+            string comment = ""
+        )
+            : this(verificationStatus, $"'{expected}' does not equal '{actual}'. {comment}".Trim())
+        {
+        }
     }
 }
